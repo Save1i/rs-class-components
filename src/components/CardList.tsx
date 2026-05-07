6ab78ef7535668e1a8db8f1005dedb8079ef6f0e
Save1i <savva0512@gmail.com>
2026-05-07 18:19:05 +0300

    feat: add component

---
+++ src/components/CardList.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import type { Pokemon } from "./Main";
+
+interface Props {
+  item: Pokemon | null,
+  error: string,
+}
+
+class CardList extends React.Component<Props, {}> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {  
+
+    };
+  }
+
+
+
+
+
+  render() {
+    const { item: pokemon, error: searchError } = this.props;
+
+    if (searchError) {
+      return <p className="error">Ошибка: {searchError}</p>;
+    }
+
+    if (!pokemon) {
+      return <p>Loading...</p>;
+    }
+    
+    return (
+      <>
+      {
+        pokemon.results && ((pokemon.results).length >= 1) && (
+          <ul>
+            {pokemon.results.map((el) => {
+              return <li key={el.name}>{el.name}</li>
+            })}
+          </ul>
+       ) } 
+      
+        <div>
+          <p>Name: {pokemon.name}</p>
+          <p>Height: {pokemon.height}</p>
+          <p>Weight: {pokemon.weight}</p>
+        </div>
+      </>
+    );
+
+  }
+}
+
+export default CardList;
