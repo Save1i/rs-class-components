749fea93ad57ec7351ccb7a1d7ca18b77fffef4d
Save1i <savva0512@gmail.com>
2026-05-25 23:43:37 +0300

    feat: use useSelectedItemsStore for setuptest

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,8 +1,10 @@
 import '@testing-library/jest-dom'
 import { afterEach, vi } from 'vitest'
+import { useSelectedItemsStore } from './store/selectedItemsStore'
 
 
 afterEach(() => {
   vi.restoreAllMocks()
   localStorage.clear()
+  useSelectedItemsStore.setState({ selectedItems: [] })
 })
