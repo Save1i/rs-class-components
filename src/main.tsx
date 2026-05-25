870fb9d173ec2456b4e331fd9b2aee824c26b1b9
Save1i <savva0512@gmail.com>
2026-05-25 23:44:04 +0300

    feat: covet app in ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { ThemeProvider } from './context/ThemeContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
 );
