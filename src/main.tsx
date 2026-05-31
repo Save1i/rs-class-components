74adee1254a917be2d94895f5c637e7eb3cd200e
Save1i <savva0512@gmail.com>
2026-05-31 19:47:54 +0300

    feat: add defaultOptions for queryClient

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,31 @@
 import { BrowserRouter } from 'react-router-dom';
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { ThemeProvider } from './context/ThemeContext.tsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient } from '@tanstack/react-query';
 
-  const queryClient = new QueryClient()
+const QUERY_CACHE_TIME  = 5 * 60 * 1000;
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: QUERY_CACHE_TIME ,
+        gcTime: QUERY_CACHE_TIME ,
+        retry: false,
+      },
+    },
+  });
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <BrowserRouter>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </BrowserRouter>
-    </QueryClientProvider>
-  // </StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </QueryClientProvider>
 );
