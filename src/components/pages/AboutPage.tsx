37669e650e729a169de15cf08182735162558cc0
Save1i <savva0512@gmail.com>
2026-06-08 23:56:50 +0300

    feat: create forms

--- src/components/pages/AboutPage.tsx
+++ src/components/pages/AboutPage.tsx
@@ -2,9 +2,17 @@ function AboutPage() {
   return (
     <section className="about-page">
       <h1 className="title">About</h1>
-      <p className="description-text">Author: <a href="https://github.com/Save1i" target="_blank">Saveli</a></p>
+      <p className="description-text">
+        Author:{' '}
+        <a href="https://github.com/Save1i" target="_blank" rel="noreferrer">
+          Saveli
+        </a>
+      </p>
       <p className="about-text">
-        Course: <a href="https://rs.school/courses/reactjs" target="_blank">RS School React Course</a>
+        Course:{' '}
+        <a href="https://rs.school/courses/reactjs" target="_blank" rel="noreferrer">
+          RS School React Course
+        </a>
       </p>
     </section>
   );
