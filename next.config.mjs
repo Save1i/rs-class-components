f7a2f3da6594b786b4ce8a62b79e7e6ba07faac1
Save1i <savva0512@gmail.com>
2026-06-21 19:00:03 +0300

    feat: add next-intl

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Outputs a Single-Page Application (SPA).
+  // output: 'export', // Outputs a Single-Page Application (SPA).
   distDir: './dist', // Changes the build output directory to `./dist/`.
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
 }
  
-export default nextConfig
+export default withNextIntl(nextConfig)
