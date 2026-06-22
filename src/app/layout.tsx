0224e537cbb98fb73f321a370b26bd65788ae3fa
Save1i <savva0512@gmail.com>
2026-06-22 16:54:47 +0300

    feat: remove unnecessary Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
 import '../index.css';
-import { Metadata } from 'next';
 import {getLocale} from 'next-intl/server';
 import { ClientOnly } from './client';
-
-export const metadata: Metadata = {
-  title: 'React 2026 Q2',
-  description: 'My App for RS School course',
-};
 
 export default async function RootLayout({
   children,
