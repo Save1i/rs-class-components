cd90b242312adcdb4c5d2b07163f617d209e8530
Save1i <savva0512@gmail.com>
2026-06-22 16:53:47 +0300

    feat: add image allow url

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,17 @@ const nextConfig = {
   // output: 'export', // Outputs a Single-Page Application (SPA).
   distDir: './dist', // Changes the build output directory to `./dist/`.
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/PokeAPI/sprites/master/sprites/pokemon/**',
+        search: '',
+      },
+    ],
+  },
+};
  
 export default withNextIntl(nextConfig)
